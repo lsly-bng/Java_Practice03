e539fb624d9c4d7e19eb2f6c3399814ab7adb9c2
bhw <wyhoys92@gmail.com>
2022-04-19 10:18:30 +0900

    2022-04-19 practice03_ex11

--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -12,8 +12,17 @@ public class Ex11 {
 		int num=sc.nextInt();
 		int sum=0;
 		
-		
-		
+		if (num%2==0) {
+			for (int a=0; a<=num; a+=2) {
+				sum+=a;
+			}
+		}
+		else if (num%2==1) {
+			for (int a=1; a<=num; a+=2) {
+				sum+=a;
+			}
+		}
+		System.out.println("결과값: "+sum+" 입니다.");
 		sc.close();
 	}
 }
